bccaeca687e4f71ab7278c7585a529b30feae434
Denis Budyak <budden73@gmail.com>
2019-02-11 18:02:43 +0300

    Trying to make it build under MinGW, partial success

--- script/build-libgit2.sh
+++ script/build-libgit2.sh
@@ -2,12 +2,22 @@
 
 set -ex
 
-git clone --depth 1 --single-branch git://github.com/cloudson/libgit2 libgit2
+git clone --depth 1 --single-branch git://github.com/cloudson/libgit2 libgit2 
 
 cd libgit2
+# Those files are temporary, should not be in git. To minimize 
+# impact, we'll delete them now
+rm -rf CMakeCache.txt CMakeFiles 
+
+# FIXME this -G must be conditionalized 
 cmake -DTHREADSAFE=ON \
       -DBUILD_CLAR=OFF \
+      -G "MSYS Makefiles" \
       -DCMAKE_INSTALL_PREFIX=$PWD/install \
       .
 
-make install
+cmake --build .
+
+cmake --build . --target install
+
+# make install
