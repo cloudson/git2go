f3b40fbf856dd806bd85f8dfea558d451790e321
Claudson Oliveira <claudsonweb@gmail.com>
2019-10-08 00:16:48 +0100

    Use old version of libgit2

--- script/install-libgit2.sh
+++ script/install-libgit2.sh
@@ -1,22 +1,39 @@
-#!/bin/sh
-
-#
-# Install libgit2 to git2go in dynamic mode on Travis
-#
+#!/bin/bash
 
 set -ex
 
-# We don't want to build libgit2 on the next branch, as we carry a
-# submodule with the exact version we support
-if [ "x$TRAVIS_BRANCH" = "xnext" ]; then
-    exit 0
+git clone --depth 1 --single-branch git://github.com/cloudson/libgit2 libgit2 
+
+cd libgit2
+# Those files are temporary, should not be in git. To minimize 
+# impact, we'll delete them now
+rm -rf CMakeCache.txt CMakeFiles 
+
+# There is no official way to specify a generator
+# inside CMakeLists.txt, see 
+# https://stackoverflow.com/questions/11269833/cmake-selecting-a-generator-within-cmakelists-txt
+# So we deriving the environment from uname and setting generator
+# in the command line, 
+# https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+# Passing an argument with a space is a trouble, so we switch from sh to bash
+# https://stackoverflow.com/a/2249967/9469533
+EXTRA_ARGS=( )
+if [[ $machine == MinGw ]]
+then
+    extra_args=( -G "MSYS Makefiles" )
 fi
 
-cd "${HOME}"
-LG2VER="0.24.0"
-wget -O libgit2-${LG2VER}.tar.gz https://github.com/libgit2/libgit2/archive/v${LG2VER}.tar.gz
-tar -xzvf libgit2-${LG2VER}.tar.gz
-cd libgit2-${LG2VER} && mkdir build && cd build
-cmake -DTHREADSAFE=ON -DBUILD_CLAR=OFF -DCMAKE_BUILD_TYPE="RelWithDebInfo" .. && make && sudo make install
-sudo ldconfig
-cd "${TRAVIS_BUILD_DIR}"
+cmake -DTHREADSAFE=ON -DBUILD_CLAR=OFF -DCMAKE_INSTALL_PREFIX=$PWD/install "${extra_args[@]}" .
+
+cmake --build .
+
+cmake --build . --target install
