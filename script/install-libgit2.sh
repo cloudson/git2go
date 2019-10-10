ebb4727e7fd02482f94af632959900fafa24ab70
Claudson Oliveira <claudsonweb@gmail.com>
2019-10-10 20:45:47 +0100

    wip

--- script/install-libgit2.sh
+++ script/install-libgit2.sh
@@ -18,5 +18,4 @@ wget -O libgit2-${LG2VER}.tar.gz https://github.com/libgit2/libgit2/archive/v${L
 tar -xzvf libgit2-${LG2VER}.tar.gz
 cd libgit2-${LG2VER} && mkdir build && cd build
 cmake -DTHREADSAFE=ON -DBUILD_CLAR=OFF -DCMAKE_BUILD_TYPE="RelWithDebInfo" .. && make && sudo make install
-sudo ldconfig
-cd "${TRAVIS_BUILD_DIR}"
+cd -
